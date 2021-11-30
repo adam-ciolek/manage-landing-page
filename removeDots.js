993a47b4ad35d84cd6e90a0be808dfa3b7732d24
Adam Ciołek <adasciolek@gmail.com>
2021-11-30 17:38:20 +0100

    Add jq, hamburger and slider

---
+++ removeDots.js
@@ -0,0 +1,13 @@
+var dotNums = document.querySelectorAll(".slick-dots button");
+
+dotNums.forEach(el => {
+    el.classList.add('dots')
+    el.textContent = ''
+});
+
+// function removeText(item) {
+//     item.innerHTML = "";
+// }
+
+// dotNums.forEach(removeText);
+
